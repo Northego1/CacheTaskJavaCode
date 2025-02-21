ea917b75799dd568544311447ced5a1d2731b8d0
northego1 <meleshuk0804@gmail.com>
2025-02-21 10:34:44 +0300

    added data handling

--- tasks/module_3/data_handling/data_handling.py
+++ tasks/module_3/data_handling/data_handling.py
@@ -1,9 +1,12 @@
+import asyncio
+import json
 import random
 import time
 from concurrent.futures import ThreadPoolExecutor
 from itertools import chain
 from multiprocessing import Pool, Process, Queue, cpu_count
-from multiprocessing import pool as thr_pool
+
+import aiofiles
 
 
 def generate_data(n: int) -> list[int]:
@@ -18,48 +21,37 @@ def process_number(number: int) -> int:
 
 
 def run_with_tread_pool(n: int, thread_pool_size: int = 5) -> list[int]:
-    def collect_data(executor: ThreadPoolExecutor) -> list[int]:
-        chunks = [n // thread_pool_size] * thread_pool_size
-        for i in range(n % thread_pool_size):
-            chunks[i] += 1
+    def processing(chunk: int) -> list[int]:
+        data = generate_data(chunk)
+        return [process_number(num) for num in data]
 
-        futures = [
-            executor.submit(generate_data, chunk)
-            for chunk in chunks
-        ]
-        return list(chain.from_iterable(f.result() for f in futures))
-
-
-    def proccess_data(executor: ThreadPoolExecutor, data: list[int]) -> list[int]:
-        result = executor.map(process_number, data)
-        return list(result)
-
+    result = []
+    chunks = [n // thread_pool_size] * thread_pool_size
+    for i in range(n % thread_pool_size):
+        chunks[i] += 1
 
     with ThreadPoolExecutor(max_workers=thread_pool_size) as executor:
-        data = collect_data(executor)
-        processed_data = proccess_data(executor, data)
+        futures = [
+            executor.submit(processing, chunk) for chunk in chunks
+        ]
+        result = list(chain(*[f.result() for f in futures]))
         executor.shutdown(wait=False)
-        return processed_data
+        return result
 
+
+def _pool_process_processing(chunk: int) -> list[int]:
+    data = generate_data(chunk)
+    return [process_number(num) for num in data]
 
 def run_with_process_pool(n: int) -> list[int]:
-    def collect_data(pool: thr_pool.Pool) -> list[int]:
-        chunks = [[n // cpu_cores]] * cpu_cores
-        for i in range(n % cpu_cores):
-            chunks[i][0] += 1
-
-        result = pool.starmap_async(generate_data, chunks)
-        return list(chain(*result.get()))
-
-
-    def proccess_data(pool: thr_pool.Pool, data: list[int]) -> list[int]:
-        return pool.map(process_number, data)
-
-
     cpu_cores = cpu_count()
+    chunks = [[n // cpu_cores] for _ in range(cpu_cores)]
+    for i in range(n % cpu_cores):
+        chunks[i][0] += 1
+
     with Pool(processes=cpu_cores) as pool:
-        data = collect_data(pool)
-        return proccess_data(pool, data)
+        pr_result = pool.starmap_async(_pool_process_processing, chunks)
+        return list(chain(*pr_result.get()))
 
 
 def run_with_single_processes(n: int) -> list[int]:
@@ -91,30 +83,61 @@ def run_with_single_processes(n: int) -> list[int]:
     return result
 
 
-def main(
+async def main(
         n: int,
         thread_pool_size: int = 8,
+        saving_path: str = './results.json',
 ) -> None:
+    run_s_pr_time = time.time()
+    result = [process_number(num) for num in generate_data(n)]
+    run_s_pr_result = time.time()- run_s_pr_time
+
     run_thr_st_time = time.time()
     run_with_tread_pool(n, thread_pool_size=thread_pool_size)
     run_thr_result = time.time() - run_thr_st_time
 
     run_pr_st_time = time.time()
     run_with_process_pool(n)
+    run_pr_pool_result = time.time() - run_pr_st_time
+
+    run_pr_st_time = time.time()
+    run_with_single_processes(n)
     run_pr_result = time.time() - run_pr_st_time
 
-    run_pr_s_st_time = time.time()
-    run_with_single_processes(n)
-    run_pr_s_result = time.time() - run_pr_s_st_time
 
-    print(
-        'threads', run_thr_result, '\n',
-        'pr_pool', run_pr_result, '\n',
-        'pr_single', run_pr_s_result
-    )
+    result = {
+        "test_info": {
+            "n": n,
+            "thread_pool_size": thread_pool_size,
+            "process_pool_size": "equal cpu cores",
+            "processes_quantity": "equal cpu cores",
+        },
+        "test_results": {
+            "SingleProcessResult": {
+                "exec_time": round(run_s_pr_result, 5),
+                "%": 0,
+            },
+            "ThreadPoolResult": {
+                "exec_time": round(run_thr_result, 5),
+                "%": round((run_s_pr_result / run_thr_result) * 100, 1),
+            },
+            "ProcessPoolResult": {
+                "exec_time": round(run_pr_pool_result, 5),
+                "%": round((run_s_pr_result / run_pr_pool_result) * 100, 1),
+            },
+            "ProcessResult": {
+                "exec_time": round(run_pr_result, 5),
+                "%": round((run_s_pr_result / run_pr_result) * 100, 1),
+            },
+        },
+    }
+
+    async with aiofiles.open(saving_path, "w", encoding="utf-8") as file:
+        await file.write(json.dumps(result, indent=4))
+
 
 
 if __name__ == "__main__":
-    main(100_00)
+    asyncio.run(main(100_000))
 
 
