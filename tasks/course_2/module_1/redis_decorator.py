147ef30da10a9b47c1dfbbbd8a255e3e4bebd56d
northego1 <meleshuk0804@gmail.com>
2025-03-06 10:52:38 +0300

    added drf_auth

--- tasks/course_2/module_1/redis_decorator.py
+++ tasks/course_2/module_1/redis_decorator.py
@@ -1,28 +1,45 @@
 import datetime
+import multiprocessing
 import time
+from functools import wraps
+from queue import Empty
 from typing import Any, Callable
 
-import redis as r
+from redis import Redis, lock
 
+redis = Redis()
 
 def single(max_processing_time: datetime.timedelta) -> Callable[..., Any]:
     def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
-        def _is_already_running() -> bool:
-            flag = redis.get("single_running")
-            return bool(flag)
+        lck = lock.Lock(
+            redis,
+            "single_running_lock",
+            timeout=max_processing_time.total_seconds(),
+        )
+        queue = multiprocessing.Queue()
+        def _pr_func(
+                foo: Callable[..., Any],
+                queue: multiprocessing.Queue,
+                args: tuple, kwargs: dict,
+        ) -> None:
+            result = foo(*args, **kwargs)
+            queue.put(result)
 
-
+        @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
-            if not _is_already_running():
-                redis.setex(
-                    "single_running",
-                    int(max_processing_time.total_seconds()),
-                    "1",
-                )
-                result = func(*args, **kwargs)
-                redis.delete("single_running")
-                return result
-            raise RuntimeError("single already running")
+            if not lck.acquire(blocking=False):
+                raise RuntimeError("single already running")
+            try:
+                pr = multiprocessing.Process(target=_pr_func, args=(func, queue, args, kwargs))
+                pr.start()
+                try:
+                    result = queue.get(timeout=max_processing_time.total_seconds())
+                except Empty:
+                    pr.terminate()
+                    raise RuntimeError("unfe")
+            finally:
+                lck.release()
+            return result
         return wrapper
     return decorator
 
@@ -40,5 +57,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    redis = r.Redis()
     main()
